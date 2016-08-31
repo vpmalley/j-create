120448feec5cbfbcc19ee6eaeb32b4a1b5a62bc8
Vincent Malley <vincent.malley@insalien.org>
2016-09-01 00:48:25 +0200

    trying to match agera spirit

--- app/src/main/java/com/jetlag/jcreator/activity/chatstory/ChatStoryPresenter.java
+++ app/src/main/java/com/jetlag/jcreator/activity/chatstory/ChatStoryPresenter.java
@@ -1,11 +1,14 @@
 package com.jetlag.jcreator.activity.chatstory;
 
+import android.support.annotation.NonNull;
+
+import com.google.android.agera.Merger;
 import com.google.android.agera.MutableRepository;
 import com.google.android.agera.Repositories;
+import com.google.android.agera.Repository;
 import com.google.android.agera.Updatable;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by vince on 31/08/16.
@@ -13,10 +16,27 @@ import java.util.List;
 public class ChatStoryPresenter implements ChatStoryActions {
 
   private MutableRepository<String> nextParagraphTextRepo;
-  private ParagraphsRepository paragraphsRepo;
+  private ParagraphsUpdatable paragraphsUpdatable;
+  private MutableRepository<ArrayList<String>> paragraphsRepo;
+  private Repository<ArrayList<String>> paragraphsBuilder;
 
   public void createRepos() {
     nextParagraphTextRepo = Repositories.mutableRepository("");
+    paragraphsRepo = Repositories.mutableRepository(new ArrayList<String>());
+    paragraphsBuilder = Repositories.repositoryWithInitialValue(new ArrayList<String>())
+        .observe(nextParagraphTextRepo)
+        .onUpdatesPerLoop()
+        .getFrom(nextParagraphTextRepo)
+        .mergeIn(paragraphsRepo, new Merger<String, ArrayList<String>, ArrayList<String>>() {
+          @NonNull
+          @Override
+          public ArrayList<String> merge(@NonNull String nextParagraph, @NonNull ArrayList<String> paragraphs) {
+            paragraphs.add(nextParagraph);
+            return paragraphs;
+          }
+        })
+        .thenGetFrom(paragraphsRepo)
+        .compile();
   }
 
   public void addParagraph(String newParagraph) {
@@ -24,28 +44,25 @@ public class ChatStoryPresenter implements ChatStoryActions {
   }
 
   public void bindUpdatables(ChatStoryDisplay chatStoryDisplay) {
-    paragraphsRepo = new ParagraphsRepository(chatStoryDisplay);
-    nextParagraphTextRepo.addUpdatable(paragraphsRepo);
+    paragraphsUpdatable = new ParagraphsUpdatable(chatStoryDisplay);
+    paragraphsBuilder.addUpdatable(paragraphsUpdatable);
   }
 
   public void unbindUpdatables() {
-    nextParagraphTextRepo.removeUpdatable(paragraphsRepo);
+    paragraphsBuilder.removeUpdatable(paragraphsUpdatable);
   }
 
-  private class ParagraphsRepository implements Updatable {
-
-    private List<String> paragraphs = new ArrayList<>();
+  private class ParagraphsUpdatable implements Updatable {
 
     private final ChatStoryDisplay chatStoryDisplay;
 
-    private ParagraphsRepository(ChatStoryDisplay chatStoryDisplay) {
+    private ParagraphsUpdatable(ChatStoryDisplay chatStoryDisplay) {
       this.chatStoryDisplay = chatStoryDisplay;
     }
 
     @Override
     public void update() {
-      paragraphs.add(nextParagraphTextRepo.get());
-      chatStoryDisplay.displayParagraphs(paragraphs);
+      chatStoryDisplay.displayParagraphs(paragraphsBuilder.get());
     }
   }
 }
